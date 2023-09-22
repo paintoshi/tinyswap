d8091b54c3fc9c5e7c802f6e12d33ff829990073
paintoshi <88572775+paintoshi@users.noreply.github.com>
2023-09-22 12:02:13 +0200

    fix: Hosting on root domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
-const repo = 'tinyswap'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+/** Only needed if hosted on github pages URL: https://user.github.io/tinyswap/
+ * const repo = 'tinyswap'
+ * const assetPrefix = `/${repo}/`
+ * const basePath = `/${repo}`
+*/
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['@lifi/widget', '@lifi/wallet-management'],
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath
+  // assetPrefix: assetPrefix,
+  // basePath: basePath
 };
 
 module.exports = nextConfig;
