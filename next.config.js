929718db18591322b0c9ca91490ccc5df191ba53
paintoshi <88572775+paintoshi@users.noreply.github.com>
2023-09-22 11:30:52 +0200

    fix: Test GH deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = 'tinyswap'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['@lifi/widget', '@lifi/wallet-management'],
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath
 };
 
 module.exports = nextConfig;
