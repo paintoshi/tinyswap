7c2062558bc4b1f9184feb69eea098c9fa6071c5
paintoshi <88572775+paintoshi@users.noreply.github.com>
2023-09-22 14:18:46 +0200

    fix: Favicon and og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '@/styles/Home.module.css'
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import { LoadingIndicator } from '../components/LoadingIndicator';
+import Script from 'next/script';
 
 const manrope = Manrope({ subsets: ['latin'] })
 
@@ -21,20 +22,57 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>TinySwap</title>
-        <meta name="description" content="A simple but powerful DeFi Swap" />
+        <meta name="description" content="A very simple but powerful DeFi Swap interface, powered by LI.FI." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta name="keywords" content="defi, swap, crypto, lifi, li.fi, exchange, metamask" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="TinySwap - The Simplest Crypto Swap Ever!" />
+        <meta name="twitter:image" content="https://tinyswap.app/og.png" />
+        <meta name="twitter:domain" content="tinyswap.app" />
+        <meta name="twitter:site" content="@paintoshi" />
+        <meta name="twitter:creator" content="@paintoshi" />
+        <meta
+          name="twitter:description"
+          content="A very simple but powerful DeFi Swap interface, powered by LI.FI."
+        />
+
+        <meta property="og:title" content="TinySwap - The Simplest Crypto Swap Ever!" />
+        <meta property="og:description" content="A very simple but powerful DeFi Swap interface, powered by LI.FI." />
+        <meta property="og:image" content="https://tinyswap.app/og.png" />
+        <meta property="og:url" content="https://tinyswap.app" />
       </Head>
       <main className={`${styles.main} ${manrope.className}`}>
         <div className={styles.center}>
           <h1 className={styles.title}>TinySwap</h1>
           <p className={styles.sub}>
-            Tiny But Powerful! <br />
+            Tiny, but Powerful & Zero fees added <br />
             Hosted & Maintained by <a href="https://github.com/paintoshi/tinyswap">Paintoshi</a>
           </p>
           <LiFiWidgetNext />
         </div>
       </main>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        id="google-tag1"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CYE6KFRLTH`}
+      />
+      <Script
+        strategy="afterInteractive"
+        id="google-tag2"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CYE6KFRLTH', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
   );
 };
