4448c9f89c25d928482836eaf2260a3384b5c200
paintoshi <88572775+paintoshi@users.noreply.github.com>
2023-09-23 10:30:11 +0200

    fix: Updating config files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 /** Only needed if hosted on github pages URL: https://user.github.io/tinyswap/
+ * Also uncomment the assetPrefix and basePath below
  * const repo = 'tinyswap'
  * const assetPrefix = `/${repo}/`
  * const basePath = `/${repo}`
