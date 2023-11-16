ed686340cd82cfacc5f01dfde116aaeb0f276f95
paintoshi <88572775+paintoshi@users.noreply.github.com>
2023-11-16 08:08:05 +0000

    Deploying to gh-pages from @ paintoshi/tinyswap@7f8a2111bb168c42b7ad38c8751e0cbb0bce5ee6 🚀

--- _next/static/uN7m9txQvl0MCkuvfRzhk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/01cd1aaada572387.css","static/chunks/pages/index-61b845485218b86e.js"],"/_error":["static/chunks/pages/_error-a9572f84d60f21da.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
