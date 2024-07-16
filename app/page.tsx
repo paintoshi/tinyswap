426d1e84d86169185eebb3c5ad6a25d537808a5c
paintoshi <88572775+paintoshi@users.noreply.github.com>
2024-07-16 14:10:52 +0200

    fix: Styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.center}>
         <h1 className={styles.title}>TinySwap</h1>
+        <p className={styles.description}>
+          Simple and powerful Cross-chain Swap/Bridge
+        </p>
         <Widget />
       </div>
       <a className={styles.githubLink} href="https://github.com/paintoshi/tinyswap" target="_blank" rel="noopener noreferrer"><Image src="/github.svg" className={styles.githubImage} alt="Github" width={32} height={32} /></a>
